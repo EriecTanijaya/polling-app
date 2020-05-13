63842d8a35acaf878ab587f9c3e1ae2a0b0619de
Eriec Tan <eriectan88@gmail.com>
2020-05-13 23:28:47 +0700

    register : done logic

--- register/register.php
+++ register/register.php
@@ -22,4 +22,37 @@ if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['emai
     exit('Please complete the registration form');
 }
 
+// We need to check if the account with that username exists.
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+    // Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
+    $stmt->bind_param('s', $_POST['username']);
+    $stmt->execute();
+    $stmt->store_result();
+    // Store the result so we can check if the account exists in the database.
+    if ($stmt->num_rows > 0) {
+        // Username already exists
+        echo 'Username exists, please choose another!';
+    } else {
+        // Insert new account
+        // Username doesnt exists, insert new account
+        if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)')) {
+            // We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
+            // we not use hash
+            // $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+            $password = $_POST['password'];
+            $stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
+            $stmt->execute();
+            echo 'You have successfully registered, you can now login!';
+        } else {
+            // Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
+            echo 'Could not prepare statement!';
+        }
+    }
+    $stmt->close();
+} else {
+    // Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
+    echo 'Could not prepare statement!';
+}
+$con->close();
+
 ?>
