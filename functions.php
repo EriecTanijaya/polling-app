db42d2620208492fe57402c6c620e5c1dc11348e
Eriec Tan <eriectan88@gmail.com>
2020-05-13 21:41:02 +0700

    functions: add template function

--- functions.php
+++ functions.php
@@ -13,3 +13,34 @@ function pdo_connect_mysql()
         die('Failed to connect to database!');
     }
 }
+
+// Template header
+function template_header($title)
+{
+    echo <<<EOT
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>$title</title>
+            <link href="style.css" rel="stylesheet" type="text/css">
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
+        </head>
+        <body>
+        <nav class="navtop">
+            <div>
+                <h1>Voting & Poll System</h1>
+                <a href="index.php"><i class="fas fa-poll-h"></i>Polls</a>
+            </div>
+        </nav>
+    EOT;
+}
+
+// Template footer
+function template_footer()
+{
+    echo <<<EOT
+        </body>
+    </html>
+    EOT;
+}
