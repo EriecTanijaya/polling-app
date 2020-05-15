1385765348b5632adb424f57a79ff242bde24895
Eriec Tanijaya <eriectan88@gmail.com>
2020-05-15 14:36:15 +0700

    update phppoll.sql

--- phppoll.sql
+++ phppoll.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2020 at 06:04 PM
+-- Generation Time: May 15, 2020 at 09:35 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.5
 
@@ -31,8 +31,19 @@ CREATE TABLE `accounts` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `email` varchar(100) NOT NULL
+  `email` varchar(100) NOT NULL,
+  `npm` varchar(255) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `prodi` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`id`, `username`, `password`, `email`, `npm`, `ip`, `prodi`) VALUES
+(25, 'eriectan', '$2y$10$s9hrcnOwC60Ju8356WcsKuO4UWeRoLMtVP.DwPBsl.UI2E.ZItJO.', 'eriectan88@gmail.com', '1831100', '::1', 'Sistem Informasi'),
+(26, 'admin', '$2y$10$ced/R1Qq37BQHuW/XutTNelzxdZGW9jmLxZ0DAL0cn2FZvfit4J8q', 'admin@admin.com', '000000', '::1', 'admin');
 
 -- --------------------------------------------------------
 
@@ -51,7 +62,7 @@ CREATE TABLE `polls` (
 --
 
 INSERT INTO `polls` (`id`, `title`, `desc`) VALUES
-(1, 'What\'s your favorite programming language?', '');
+(3, 'Apakah kamu kangen kuliah normal seperti biasa?', 'Kuliah normal atau kuliah online');
 
 -- --------------------------------------------------------
 
@@ -71,10 +82,28 @@ CREATE TABLE `poll_answers` (
 --
 
 INSERT INTO `poll_answers` (`id`, `poll_id`, `title`, `votes`) VALUES
-(1, 1, 'PHP', 1),
-(2, 1, 'Python', 0),
-(3, 1, 'C#', 0),
-(4, 1, 'Java', 0);
+(8, 3, 'kangen kuliah normal', 1),
+(9, 3, 'udah pw kuliah online', 0),
+(10, 3, 'ha? apa itu kuliah?', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `poll_commit`
+--
+
+CREATE TABLE `poll_commit` (
+  `id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `poll_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `poll_commit`
+--
+
+INSERT INTO `poll_commit` (`id`, `account_id`, `poll_id`) VALUES
+(6, 25, 3);
 
 --
 -- Indexes for dumped tables
@@ -99,6 +128,14 @@ ALTER TABLE `poll_answers`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `poll_commit`
+--
+ALTER TABLE `poll_commit`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `account_id` (`account_id`),
+  ADD KEY `poll_id` (`poll_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -106,19 +143,36 @@ ALTER TABLE `poll_answers`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `polls`
 --
 ALTER TABLE `polls`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `poll_answers`
 --
 ALTER TABLE `poll_answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `poll_commit`
+--
+ALTER TABLE `poll_commit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `poll_commit`
+--
+ALTER TABLE `poll_commit`
+  ADD CONSTRAINT `account_id` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`),
+  ADD CONSTRAINT `poll_id` FOREIGN KEY (`poll_id`) REFERENCES `polls` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
