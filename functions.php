0f9ca4b7529abec330105a3c6bdfad98f61d6ca4
Eriec Tan <eriectan88@gmail.com>
2020-05-18 17:34:02 +0700

    make the host server happy

--- functions.php
+++ functions.php
@@ -17,13 +17,12 @@ function pdo_connect_mysql()
 // Template header
 function template_header($title)
 {
-    echo <<<EOT
-    <!DOCTYPE html>
+    echo '<!DOCTYPE html>
     <html>
         <head>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>$title</title>
+            <title>' . $title . '</title>
             <link rel="shortcut icon" href="favicon.ico" />
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
             <link href="style.css" rel="stylesheet" type="text/css">
@@ -32,7 +31,7 @@ function template_header($title)
         <body>
         <nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark">
             <i class="fa fas fa-poll-h fa-2x" style="margin-right: 10px; color: #ffffff"></i>
-            <a class="navbar-brand display-1 font-weight-bold" href="index.php">Voting 'n Polling</a>
+            <a class="navbar-brand display-1 font-weight-bold" href="index.php">Voting \'n Polling</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -66,23 +65,20 @@ function template_header($title)
                     </li>
                 </ul>
             </div>
-        </nav>
-    EOT;
+        </nav>';
 }
 
 // Template footer
 function template_footer()
 {
-    echo <<<EOT
-        <footer class="footer">
+    echo '<footer class="footer">
             <span class="text-center align-middle">Made with ❤️ in Batam</span>
         </footer>
             <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
             <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
         </body>
-    </html>
-    EOT;
+    </html>';
 }
 
 ?>
